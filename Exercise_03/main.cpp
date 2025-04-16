2ecca71c1c66205c696f39ecc7688a7de6bc5cc4
Haris <s2310237010@students.fh-hagenberg.at>
2025-04-16 11:36:44 +0200

    finished Exercise 03

--- Exercise_03/main.cpp
+++ Exercise_03/main.cpp
@@ -1,12 +1,35 @@
 #include "list.h"
+#include "person.h"
 #include <cassert>
+
+void print() {
+
+}
 
 int main() {
 
     MC::List<int> list1;
     assert(list1.size() == 0);
-    list1.push_back(100);
+    list1.push_back(400);
     assert(list1.size() == 1);
-    assert(list1.back() == 100);
+    assert(list1.back() == 400);
+    list1.push_back(300);
+    list1.push_back(200);
+    list1.push_back(100);
+
+    list1.print();
+    std::cout << std::endl;
+
+    MC::List<Person> list2;
+    list2.push_back(Person{ "John Doe" });
+    list2.push_back(Person{ "Jane Doe" });
+
+    list2.print();
+    std::cout << std::endl;
+
+    list2.sort();
+
+    list2.print();
+    std::cout << std::endl;
 
 }
